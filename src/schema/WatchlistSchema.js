3781b3b64352d323e963b4b244a4dbe48c73dae9
Aditya Singh <singh.aditya.44618@gmail.com>
2026-04-18 11:46:54 +0530

    added new field and modified schema

--- src/schema/WatchlistSchema.js
+++ src/schema/WatchlistSchema.js
@@ -1,14 +1,15 @@
 const { Schema } = require("mongoose");
 
 const WatchlistSchema = new Schema({
-  email: { type: String, required: true },
-  symbol: { type: String, required: true },
-  high: Number,
-  close: Number,
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  symbol: { type: String, required: true, uppercase: true, trim: true },
+  high: { type: Number, default: 0 },
+  low: { type: Number, default: 0 },
+  close: { type: Number, default: 0 },
 }, { timestamps: true });
 
 WatchlistSchema.index(
-  { email: 1, symbol: 1 },
+  { userId: 1, symbol: 1 },
   { unique: true }
 );
 
