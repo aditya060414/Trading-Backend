497262f588e3e7ed9f700f9611e68c9b7ac7f727
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-20 16:28:10 +0530

    deleted schema

--- src/models/PositionsModel.js
+++
@@ -1,7 +0,0 @@
-const { model } = require("mongoose");
-
-const { PositionsSchema } = require("../schema/PositionsSchema");
-
-const PositionsModel = new model("position", PositionsSchema);
-
-module.exports = { PositionsModel };
