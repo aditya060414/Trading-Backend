8c3d2d284f5b9dc8a65e1096dfce290a02455f20
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-21 17:25:25 +0530

    function explaintaion

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 
-// Main application database
+
 const connectDB = async () => {
   try {
-    // const mongoUrl = process.env.MONGO_URL;
+    // import of  mongo url from environment varaibles
     const mongoUrl = process.env.MONGO_URL;
+    // if there is no url, i.e empty url then error is thrown
     if (!mongoUrl) {
       throw new Error("MONGO_URL environment variable is not defined");
     }
+    // if url is present, then database connection starts
     await mongoose.connect(mongoUrl);
     console.log("MongoDB Connected Successfully");
   } catch (err) {
+    // if connection fails, then error is logged and process is exited
     console.error("MongoDB connection error:", err.message);
-    process.exit(1); // Exit process with failure
+    // when connection fails then nothing is fetched from database, so instead of showing broken server kill the process completely
+    process.exit(1);
   }
 };
 
