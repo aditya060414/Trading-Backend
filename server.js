e03672655c9723f0de1396305ac374baa29c42ae
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-21 17:24:53 +0530

    function explaintaion

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
-const { connectDB } = require('./src/config/db'); // Import modular DB connection
-const app = require('./app'); // import express app
+//import of database connection fuction
+const { connectDB } = require('./src/config/db'); 
+// import express app from app.js
+const app = require('./app'); 
+// it is function which is used to fetch stock data from the api and update the database.
 const { fetchStock } = require('./src/controllers/PortfolioController');
+// defines port number initialized in .env file
 const PORT = process.env.PORT || 8000;
 
 // Connect DB and start server
