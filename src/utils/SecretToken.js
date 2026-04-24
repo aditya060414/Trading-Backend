f1d4254a857f126319d995e12617146ebf7eeb6f
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-24 23:26:11 +0530

    if secret token is missing then throw error

--- src/utils/SecretToken.js
+++ src/utils/SecretToken.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-// console.log("JWT_SECRET:", process.env.TOKEN_KEY);
-const JWT_SECRET = process.env.JWT_SECRET; 
-const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+if (!JWT_SECRET || !JWT_REFRESH_SECRET) {
+  throw new Error("JWT secrets are not defined in environment variables");
+}
 /**
  * Access Token: Short-lived (e.g., 15 minutes)
  * Used for authenticating every request.
@@ -13,9 +15,10 @@ module.exports.generateAccessToken = (user) => {
   return jwt.sign(
     { id: user._id, role: user.role }, // Include role for faster authorization
     JWT_SECRET,
-    { expiresIn: "1d",
+    {
+      expiresIn: "30m",
       issuer: "MarketEx",
-     }
+    }
   );
 };
 
@@ -27,5 +30,6 @@ module.exports.generateAccessToken = (user) => {
 module.exports.generateRefreshToken = (id) => {
   return jwt.sign({ id }, JWT_REFRESH_SECRET, {
     expiresIn: "7d",
+    issuer: "MarketEx",
   });
 };
