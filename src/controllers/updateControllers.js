ddf644367452e63339b0846d6d2475aa23ff3955
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-25 22:24:11 +0530

    change username validation, mongo db update and if user exist or not

--- src/controllers/updateControllers.js
+++ src/controllers/updateControllers.js
@@ -1,8 +1,31 @@
+const User = require("../models/UserModel");
 
-module.exports.changeUsername = async(req,res)=>{
-    res.send("change username");
+
+module.exports.changeUsername = async (req, res) => {
+    const { username } = req.body.username?.trim();
+    const { id } = req.user;
+
+    if (!username) {
+        return res.status(400).json({ message: "Username is required." });
+    }
+
+    try {
+        const userInfo = await User.findOne({ _id: id });
+        if (!userInfo) {
+            return res.status(400).json({ message: "User does not exist." });
+        }
+        const updatedUser = await User.findByIdAndUpdate(id, { $set: { username } }, { new: true });
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User does not exist." });
+        }
+
+        return res.status(200).json({ message: "Updated successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to update username.", error: error.message });
+    }
+
 }
 
-module.exports.changePassword  = async(req,res)=>{
+module.exports.changePassword = async (req, res) => {
     res.send("change password");
 }
