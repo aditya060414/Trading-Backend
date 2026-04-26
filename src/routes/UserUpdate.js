3dc6aa60e0f4668ba960caba46969f17591eb336
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-26 17:41:13 +0530

    routing update and verification

--- src/routes/UserUpdate.js
+++ src/routes/UserUpdate.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { userVerification } = require('../middleware/AuthMiddleWare');
-
+const { changeUsername, changePassword } = require('../controllers/updateControllers');
 const router = express.Router();
 
 // replaces only the changed filled rest remain the same
-router.patch('/username/:id',userVerification,changeUsername);
-router.patch('/password/:id',userVerification,changePassword)
+router.patch('/username/:id', userVerification, changeUsername);
+router.patch('/password/:id', userVerification, changePassword)
 
 module.exports = router;
