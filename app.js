4a3651cbcd067fb41deb292825e883cf61ac5bb1
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-27 09:19:48 +0530

    allow patch request

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(cors({
             callback(new Error("Not allowed by CORS"));
         }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true
 }));
 
