33a60a3f2deb50584033cb5b2d91cca5987f39d0
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-27 10:34:53 +0530

    do not register duplicate username

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -16,6 +16,7 @@ const userSchema = new Schema(
     username: {
       type: String,
       required: [true, "Username is required"],
+      unique: [true, "Username is already taken."],
       trim: true,
       minlength: [3, "Username must be at least 3 characters"],
       maxlength: [30, "Username cannot exceed 30 characters"],
