ad5a982d1fa0036c06dec3243affa2bac39e049f
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-29 17:41:12 +0530

    update: removed password from getting saved in redis

--- src/controllers/updateControllers.js
+++ src/controllers/updateControllers.js
@@ -2,88 +2,100 @@ const User = require("../models/UserModel");
 const redisClient = require("../config/redis");
 
 module.exports.changeUsername = async (req, res) => {
-    const username = req.body.username?.trim();
-    const password = req.body.password;
-    const id = req.user.id;
-    
-    if (!password) {
-        return res.status(400).json({ message: "Password is required." });
+  const username = req.body.username?.trim();
+  const password = req.body.password;
+  const id = req.user.id;
+
+  if (!password) {
+    return res.status(400).json({ message: "Password is required." });
+  }
+
+  if (!username) {
+    return res.status(400).json({ message: "Username is required." });
+  }
+
+  try {
+    const user = await User.findById(id).select("+password");
+    if (!user) {
+      return res.status(400).json({
+        message: "User does not exist.",
+      });
     }
-    
-    if (!username) {
-        return res.status(400).json({ message: "Username is required." });
+    if (username === user.username) {
+      return res.status(400).json({ message: "Username cannot be same." });
     }
-    
-    
-    try {
-        const user = await User.findById(id).select("+password");
-        if (!user) {
-            return res.status(400).json({
-                message: "User does not exist."
-            })
-        }
-        if (username === user.username) {
-            return res.status(400).json({ message: "Username cannot be same." })
-        }
-        const isPasswordCorrect = await user.correctPassword(password, user.password);
-        if (!isPasswordCorrect) {
-            return res.status(401).json({ message: "Incorrect Password" });
-        }
-        const updatedUser = await User.findByIdAndUpdate(id, { $set: { username } }, { new: true });
-        if (!updatedUser) {
-            return res.status(404).json({ message: "User does not exist." });
-        }
-        // update user in redis 
-        await redisClient.del(`user:${user._id}`);
-        await redisClient.setEx(`user:${id}`, 86400, JSON.stringify(updatedUser));
-        return res.status(200).json({ message: "Username updated successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: "Failed to update username.", error: error.message });
+    const isPasswordCorrect = await user.correctPassword(
+      password,
+      user.password,
+    );
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Incorrect Password" });
     }
-    
-}
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $set: { username } },
+      { new: true },
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User does not exist." });
+    }
+    // update user in redis
+    await redisClient.del(`user:${user._id}`);
+    await redisClient.setEx(`user:${id}`, 86400, JSON.stringify(updatedUser));
+    return res.status(200).json({ message: "Username updated successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Failed to update username.", error: error.message });
+  }
+};
 
 module.exports.changePassword = async (req, res) => {
-    const newPassword = req.body.newPassword;
-    const password = req.body.password;
-    const id = req.user.id;
+  const newPassword = req.body.newPassword;
+  const password = req.body.password;
+  const id = req.user.id;
 
-    if (!newPassword || !password) {
-        return res.status(400).json({
-            message: "Password is required."
-        })
+  if (!newPassword || !password) {
+    return res.status(400).json({
+      message: "Password is required.",
+    });
+  }
+  if (newPassword === password) {
+    return res.status(400).json({
+      message: "New password cannot be same as old password.",
+    });
+  }
+  try {
+    const user = await User.findById(id).select("+password");
+    if (!user) {
+      return res.status(400).json({
+        message: "User does not exist.",
+      });
     }
-    if (newPassword === password) {
-        return res.status(400).json({
-            message: "New password cannot be same as old password."
-        })
+    const isPasswordCorrect = await user.correctPassword(
+      password,
+      user.password,
+    );
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: "Incorrect Password.",
+      });
     }
-    try {
-        const user = await User.findById(id).select("+password");
-        if (!user) {
-            return res.status(400).json({
-                message: "User does not exist."
-            })
-        }
-        const isPasswordCorrect = await user.correctPassword(password, user.password);
-        if (!isPasswordCorrect) {
-            return res.status(400).json({
-                message: "Incorrect Password."
-            })
-        }
 
+    user.password = newPassword;
+    await user.save();
+    await redisClient.del(`user:${user._id}`);
+    const userObj = user.toObject();
+    delete userObj.password;
 
-        user.password = newPassword;
-        await user.save();
-        await redisClient.del(`user:${user._id}`);
-        await redisClient.setEx(`user:${user._id}`, 86400, JSON.stringify(user));
-        return res.status(200).json({
-            message: "Password updated successfully",
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: "Password update failed.",
-            error: error.message
-        })
-    }
-}
+    await redisClient.setEx(`user:${user._id}`, 86400, JSON.stringify(userObj));
+    return res.status(200).json({
+      message: "Password updated successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Password update failed.",
+      error: error.message,
+    });
+  }
+};
