745922e419b605ce3a1ced9b525e8f193399400f
aditya060414 <singh.aditya.44618@gmail.com>
2026-05-01 14:33:24 +0530

    server: remove fetch stocks

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ require("dotenv").config();
 const { connectDB } = require('./src/config/db'); 
 // import express app from app.js
 const app = require('./app'); 
-// it is function which is used to fetch stock data from the api and update the database.
-const { fetchStock } = require('./src/controllers/PortfolioController');
 // defines port number initialized in .env file
 const PORT = process.env.PORT || 8000;
 
+
 // Connect DB and start server
 connectDB().then(async () => {
-
-    await fetchStock();
 
     app.listen(PORT, () => {
         console.log(`Server is listening on ${PORT}`);
